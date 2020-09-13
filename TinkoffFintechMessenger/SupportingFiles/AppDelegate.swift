093dbdace939304fad7a8f8cf74e7600362f2dda
NikitaGundorin <djnig@yandex.ru>
2020-09-13 18:49:01 +0300

    HW-1 Initial settings

---
+++ TinkoffFintechMessenger/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TinkoffFintechMessenger
+//
+//  Created by Nikita Gundorin on 13.09.2020.
+//  Copyright © 2020 Nikita Gundorin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
