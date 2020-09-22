fe90a7b9ef78d92cf3e9d10685e4f4a9c02c7355
NikitaGundorin <djnig@yandex.ru>
2020-09-23 00:12:03 +0300

    HW-2 Add changing profile image feature

--- TinkoffFintechMessenger/Controllers/Profile/ProfileViewController.swift
+++ TinkoffFintechMessenger/Controllers/Profile/ProfileViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MobileCoreServices
+import AVFoundation
 
 final class ProfileViewController: UIViewController {
     
@@ -17,20 +19,28 @@ final class ProfileViewController: UIViewController {
     @IBOutlet private weak var profileImageLabel: UILabel!
     @IBOutlet private weak var saveButton: UIButton!
     @IBOutlet private weak var userNameLabel: UILabel!
-    @IBOutlet weak var userDescriptionLabel: UILabel!
+    @IBOutlet private weak var userDescriptionLabel: UILabel!
     
     // MARK: - Private properties
     
     private let loggerSourceName = "ProfileViewController"
     private var currentState = UIViewController.State.loading
     private let buttonCornerRadius: CGFloat = 14
+    private lazy var imagePickerController: UIImagePickerController = {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.allowsEditing = true
+        vc.delegate = self
+        vc.mediaTypes = [kUTTypeImage as String]
+        return vc
+    }()
     
     // MARK: - Initializer
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         
-        //Logger.info(loggerSourceName, "\(profileImageEditButton.frame)")
+        //Logger.info(loggerSourceName, "\(saveButton.frame)")
         //This throws an exception, because UI elements have not been loaded yet
     }
     
@@ -46,7 +56,7 @@ final class ProfileViewController: UIViewController {
                          methodName: #function)
         currentState = newState
         
-        Logger.info(loggerSourceName, "\(profileImageEditButton.frame)")
+        Logger.info(loggerSourceName, "\(saveButton.frame)")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -70,7 +80,7 @@ final class ProfileViewController: UIViewController {
                          methodName: #function)
         currentState = newState
         
-        Logger.info(loggerSourceName, "\(profileImageEditButton.frame)")
+        Logger.info(loggerSourceName, "\(saveButton.frame)")
         
         //When the viewDidLoad is called constraints of view controller's view subviews
         //are not properly set and its sizes are not finalised
@@ -117,7 +127,20 @@ final class ProfileViewController: UIViewController {
     // MARK: - IBActions
     
     @IBAction private func profileImageEditButtonPressed(_ sender: Any) {
-        //set profile image
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+                
+        let actions = [
+            UIAlertAction(title: "Open Gallery", style: .default) { [unowned self] _ in
+                self.presentImagePicker(sourceType: .photoLibrary)
+            },
+            UIAlertAction(title: "Take Photo", style: .default) { [unowned self] _ in
+                self.checkCameraPermission()
+            },
+            UIAlertAction(title: "Cancel", style: .cancel)]
+
+        actions.forEach { alertController.addAction($0) }
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     // MARK: - Private methods
@@ -126,5 +149,58 @@ final class ProfileViewController: UIViewController {
         profileImageView.layer.cornerRadius = profileImageView.frame.width / 2
         saveButton.layer.cornerRadius = buttonCornerRadius
         profileImageLabel.isHidden = profileImageView.image != nil
+    }
+    
+    func checkCameraPermission() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            return presentImagePicker(sourceType: .camera)
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    self.presentImagePicker(sourceType: .camera)
+                } else {
+                    self.showErrorAlert()
+                }
+            }
+        case .denied, .restricted:
+            showErrorAlert()
+        @unknown default:
+            showErrorAlert()
+        }
+    }
+    
+    private func presentImagePicker(sourceType: UIImagePickerController.SourceType) {
+        if !UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            showErrorAlert()
+            return
+        }
+        
+        imagePickerController.sourceType = sourceType
+        present(self.imagePickerController, animated: true)
+    }
+    
+    private func showErrorAlert() {
+        let alertController = UIAlertController(title: "Error", message: "This action is not allowed.", preferredStyle: .alert)
+        alertController.addAction(.init(title: "OK", style: .cancel, handler: nil))
+        present(alertController, animated: true)
+    }
+}
+
+// MARK: -  UINavigationControllerDelegate, UIImagePickerControllerDelegate
+
+extension ProfileViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.editedImage] as? UIImage else {
+            showErrorAlert()
+            return
+        }
+        
+        profileImageView.image = image
+        profileImageLabel.isHidden = true
     }
 }
